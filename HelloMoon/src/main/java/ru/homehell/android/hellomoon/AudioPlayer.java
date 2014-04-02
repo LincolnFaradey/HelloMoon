1374f733a63ea1f7d160c9ee0f2c30eba1dfc397
LincolnFaradey <iphoenix179@gmail.com>
2014-04-02 16:11:20 -0400

    videoView -1

--- HelloMoon/src/main/java/ru/homehell/android/hellomoon/AudioPlayer.java
+++ HelloMoon/src/main/java/ru/homehell/android/hellomoon/AudioPlayer.java
@@ -1,16 +1,19 @@
 package ru.homehell.android.hellomoon;
 
+
 import android.content.Context;
+;
 import android.media.MediaPlayer;
 
-import java.io.IOException;
+
 
 /**
  * Created by faradey on 31.03.14.
  */
 public class AudioPlayer {
     private MediaPlayer mPlayer;
-    private int pos;
+    private int pos = 0;
+
 
     public void stop() {
 
@@ -21,23 +24,30 @@ public class AudioPlayer {
         }
     }
 
-    public void play(Context c) {
-        stop();
+    public void play(final Context c) {
+        if (mPlayer != null) mPlayer.release();
         mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
 
         mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 stop();
+
             }
         });
+
+        mPlayer.seekTo(pos-1);
         mPlayer.start();
+
     }
 
     public void pause(){
-        if (mPlayer.isPlaying() && mPlayer != null){
+
+        if (mPlayer != null){
             mPlayer.pause();
+            pos = mPlayer.getCurrentPosition();
         }
+
     }
 
 
--- HelloMoon/src/main/java/ru/homehell/android/hellomoon/HelloMoonFragment.java
+++ HelloMoon/src/main/java/ru/homehell/android/hellomoon/HelloMoonFragment.java
@@ -1,12 +1,13 @@
 package ru.homehell.android.hellomoon;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.VideoView;
 
 /**
  * Created by faradey on 31.03.14.
@@ -17,15 +18,29 @@ public class HelloMoonFragment extends Fragment {
     private Button mPauseButton;
     private Button mStopButton;
 
+    private VideoView mVideoView;
+    Uri resourcesUri = Uri.parse("android.resources://" +
+            "ru.homehell.android.hellomoon/raw/apollo_17_stroll");
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
 
+        mVideoView = (VideoView) v.findViewById(R.id.videoView);
+        mVideoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView.setVideoURI(resourcesUri);
+                mVideoView.start();
+            }
+        });
+
         mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 mPlayer.play(getActivity());
             }
         });
